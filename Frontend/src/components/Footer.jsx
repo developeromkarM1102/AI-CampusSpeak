567d2d11e6551f230dbeb82deb3c374569adab73
Omkar Mutal <omkarmutal1102@gmail.com>
2026-02-12 12:05:38 +0530

    Update copyright text in Footer component

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
         <footer className='bg-blue-600 text-white text-center p-4 mt-0'>
             <p>Made with <i className="ri-heart-2-fill"></i> and Respect</p>
-            <p>&copy; 2026 KBP-CampusSpeak. All rights reserved.</p>
+            <p>&copy; 2026 AI-CampusSpeak. All rights reserved.</p>
         </footer>
     </div>
   )
